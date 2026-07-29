9a54541b9ea9b19399da375d574b4309ab3cc910
TheKadhim <Thekadhimw@gmail.com>
2026-07-29 14:36:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # ─── Bot Configuration ───────────────────────────────────────────────────────
 
 # Get this from @BotFather on Telegram
-BOT_TOKEN = "8288899186:AAHh13seImiid5JqqHQHmoTzZhqBM0JjvLg"
+BOT_TOKEN = "x"
 
 # Your channel ID. Two formats:
 #   Public channel:  "@yourchannel"
 #   Private channel: "-1001234567890"  (get this by forwarding a msg to @userinfobot)
-CHANNEL_ID = "@kadwav"
+CHANNEL_ID = "@xKadhim"
 
 # Your Telegram user ID — you are the admin
 ADMIN_ID = 6669676460
 
-BOT_USERNAME = "FlExpFileBot"
+BOT_USERNAME = "xKadhimBot"
